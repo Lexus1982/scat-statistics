13b7ed50a564731aa02ca042d565a42640c790c9
Sidorov Alexander <asidorov84@gmail.com>
2018-04-07 17:07:15 +0300

    Старт проекта. Начальный коммит. Добавлен модуль collector.

---
+++ collector/src/main/java/me/alexand/scat/statistic/collector/Collector.java
@@ -0,0 +1,10 @@
+package me.alexand.scat.statistic.collector;
+
+/**
+ * @author asidorov84@gmail.com
+ */
+public class Collector {
+    public static void main(String[] args) {
+        System.out.println("New project");
+    }
+}
