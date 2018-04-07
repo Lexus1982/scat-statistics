ef137c154d404461c33fffabf193be8fe37b2d66
Sidorov Alexander <asidorov84@gmail.com>
2018-04-07 17:39:05 +0300

    Добавлен Spring Framework. Настройки для логирования.

--- collector/src/main/java/me/alexand/scat/statistic/collector/Collector.java
+++ collector/src/main/java/me/alexand/scat/statistic/collector/Collector.java
@@ -1,10 +1,12 @@
 package me.alexand.scat.statistic.collector;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * @author asidorov84@gmail.com
  */
 public class Collector {
     public static void main(String[] args) {
-        System.out.println("New project");
+        new ClassPathXmlApplicationContext("spring-app.xml").registerShutdownHook();
     }
 }
